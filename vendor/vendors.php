8353280c1c2628047ea1be38615636694b413405
TheMaskedFerret <themaskedferret@tmf.space>
2023-12-10 04:24:22 -0500

    vendors replace \\ with /

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -3,7 +3,7 @@
 function thephpleague_oauth_client_autoloader($class) {
     if (str_contains($class,"League\OAuth2")) {
         echo $class . '<br />';
-        include str_replace("League\OAuth2","./thephpleague/oauth2-client/src",$class) . '.php';
+        include str_replace("\\","/",str_replace("League\OAuth2","./thephpleague/oauth2-client/src",$class) . '.php');
     }
 }
 spl_autoload_register('thephpleague_oauth_client_autoloader');
