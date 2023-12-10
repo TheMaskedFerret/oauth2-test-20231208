3c3a1ef0ae2010c1c7380c0307970e944164bb59
TheMaskedFerret <themaskedferret@tmf.space>
2023-12-10 05:50:03 -0500

    Update vendors.php
    added support for guzzle and monolog subrepos

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -1,4 +1,23 @@
 <?php
+function guzzle_guzzle_autoloader($class) {
+    if (str_contains($class,"GuzzleHttp\Client")) {
+        echo $class . '<br />';
+        echo __DIR__."<br>";
+        echo $_SERVER['DOCUMENT_ROOT_DIR']."<br>";
+        include str_replace("\\","/",str_replace("GuzzleHttp\Client",$_SERVER['DOCUMENT_ROOT_DIR']."/vendor/guzzle/guzzle/src",$class) . '.php');
+    }
+}
+spl_autoload_register('guzzle_guzzle_autoloader');
+
+function Seldek_monolog_autoloader($class) {
+    if (str_contains($class,"Monolog")) {
+        echo $class . '<br />';
+        echo __DIR__."<br>";
+        echo $_SERVER['DOCUMENT_ROOT_DIR']."<br>";
+        include str_replace("\\","/",str_replace("Monolog",$_SERVER['DOCUMENT_ROOT_DIR']."/vendor/Seldek/monolog/src/Monolog",$class) . '.php');
+    }
+}
+spl_autoload_register('Seldek_monolog_autoloader');
 
 function thephpleague_oauth_client_autoloader($class) {
     if (str_contains($class,"League\OAuth2")) {
@@ -9,7 +28,6 @@ function thephpleague_oauth_client_autoloader($class) {
     }
 }
 spl_autoload_register('thephpleague_oauth_client_autoloader');
-
 
 
 ?>
